f691c94a608c8022cf2d249c3e8396e44a1f845d
Mark McLeod <markmcleod45@gmail.com>
2021-11-02 16:20:47 +0000

    Add new route for /cat-form. Switch /named-cat route to use POST instead of GET

--- app.rb
+++ app.rb
@@ -1,28 +1,16 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/' do
-  "hello!"
-end
-
-get '/secret' do
-  "Can't be that secret if you're reading this"
-end
-
-get '/ryan' do
-  "Ryan"
-end
-
-get '/mark' do
-  "Mark"
-end
-
 get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
   erb(:index)
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb(:cat_form)
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
